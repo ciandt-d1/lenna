77f77dc78276dab601f0696e294e427a942b9ff8
Rodrigo de Freitas Pereira <rodrigofp@ciandt.com>
2018-02-07 12:33:40 -0200

    Add matplotlib and seaborn dependencies

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ __version__ = re.search(
     r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  open('{}/__init__.py'.format(project_name)).read()
 ).group(1)
 
+req_list = ["google-cloud-storage","matplotlib==2.1.2","seaborn==0.8.1"]
+
 setup(
     name=project_name,
     version=__version__,
     include_package_data=True,
     packages=find_packages(),
     description='tensorflow image classification framework',
-    install_requires=[
-        "google-cloud-storage==1.6.0",
-    ]
+    install_requires=req_list
 )
