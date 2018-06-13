81210f79ab782ff65821f237b72c4a089a0337df
Rodrigo de Freitas Pereira <rodrigofp@ciandt.com>
2018-06-13 17:22:18 -0300

    Attempt to fix sphinx autodoc on readthedocs

--- conf.py
+++ conf.py
@@ -30,6 +30,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+#autodoc_mock_imports = ['tf_image_classification']
+import tf_image_classification
+
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
