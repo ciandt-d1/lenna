0adcac63fe90a7c910ff577e13708d79c001957c
Rodrigo de Freitas Pereira <rodrigofp@ciandt.com>
2018-06-13 21:04:11 -0300

    Attempt to fix sphinx autodoc on readthedocs (17)

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['tensorflow','tf_image_classification']
+MOCK_MODULES = ['tensorflow','tf_image_classification','matplotlib','numpy','pandas','scipy','seaborn']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 autodoc_mock_imports = ['tf_image_classification']
