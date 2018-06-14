56bdca69452e998f5b001303c05fb7afb120c90d
Rodrigo de Freitas Pereira <rodrigofp@ciandt.com>
2018-06-14 15:34:39 -0300

    Attempt to fix sphinx autodoc on readthedocs (40)

--- conf.py
+++ conf.py
@@ -45,14 +45,14 @@
 # # MOCK_MODULES = ['tensorflow','tf_image_classification','tf_image_classification.dataset','tf_image_classification.utils','tf_image_classification.estimator_specs','matplotlib','matplotlib.pyplot','numpy','pandas','seaborn','scipy','scipy.linalg', 'scipy.signal']
 # # sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
-import sys
-import os
-print(sys.path)
-#print(os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('.'))
-#sys.path.append(os.path.abspath('.'))
-print(sys.path)
-#print(os.getcwd())
+# import sys
+# import os
+# print(sys.path)
+# #print(os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
+# #sys.path.append(os.path.abspath('.'))
+# print(sys.path)
+# #print(os.getcwd())
 
 # autodoc_mock_imports = ['tf_image_classification']
 
